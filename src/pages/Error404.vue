67d163fd9a22e11a250ea85080a77f3fc5d2844b
abenav4 <abenav.vijayakumaran@mail.utoronto.ca>
2021-11-08 21:49:54 -0500

    initial commit!

---
+++ src/pages/Error404.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="fullscreen bg-pink text-white text-center q-pa-md flex flex-center">
+    <div>
+      <div style="font-size: 30vh">
+        ;_;
+      </div>
+
+      <div class="text-h2" style="opacity:.4">
+        oh nyo..
+      </div>
+
+      <q-btn
+        class="q-mt-xl"
+        color="white"
+        text-color="pink"
+        unelevated
+        to="/"
+        label="home"
+        no-caps
+      />
+    </div>
+  </div>
+
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Error404'
+})
+</script>
+
+    .bg-brand {
+      background: #a2aa33;
+    }
